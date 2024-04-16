30bdd9b9bbb3d2f51682a52f3c2d1d8b5448f9eb
tery999 <51335465+tery999@users.noreply.github.com>
2024-04-16 12:20:23 +0300

    fixed the sorting and shuffling
    Four options now showing shuffled. One of them is always the correct answer

--- FlagGuesser/Client/src/App.jsx
+++ FlagGuesser/Client/src/App.jsx
@@ -1,34 +1,51 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import * as styles from "./App.module.css"
 import { getAllFlags } from './Services';
 
 
 function App() {
-  const [flagsArr, setFlagsArr] = useState([]);
+  const [flagsArrShuffled, setFlagsArrShuffled] = useState([]);
+  const [choices, setChoices] = useState([]);
+  const [currentFlag, setCurrentFlag] = useState({});
   const [loaded, setLoaded] = useState(false);
-  const [currentFlag, setCurrentFlag] = useState();
+
 
   useEffect( () => {
     const getFlags = async () => {
-      debugger;
       const flags = await getAllFlags();
-      const randomIndex = Math.floor(Math.random() * flags.length);
-      const getOneFlag = flags[randomIndex];
-      flags.splice(randomIndex, 1)
-      setCurrentFlag(getOneFlag);
-      setFlagsArr(flags);
+      const shuffling = flags.sort(() => Math.random() - 0.5);
+      const choices = [...shuffling];
+      setChoices(choices);
+
+      const currentFlg = shuffling.splice(0,1)[0];
+      setFlagsArrShuffled(shuffling);
+      setCurrentFlag(currentFlg);
       setLoaded(true);
     }
     getFlags();
   }, [])
 
+  const fourChoices = [];
+  fourChoices.push(currentFlag);
+  const choicesWithoutCurrent = choices.filter( (flagObj)=> {
+    return flagObj.name !== currentFlag.name;
+  })
+  const choicesWithoutCurrentShuffled = choicesWithoutCurrent.sort(() => Math.random() - 0.5);
+  const threeLeft = choicesWithoutCurrentShuffled.slice(0,3);
+  fourChoices.push(...threeLeft);
+  const fourChoicesShuffled = fourChoices.sort(() => Math.random() - 0.5);
 
+  console.log("THIS IS CURRENT FLAG", currentFlag)
   return (
-    <div className='Container'>
+    <div className={styles.container}>
       {loaded && currentFlag &&
-        <div className='flagContainer'> {currentFlag.name}</div>
+        <div className='flagContainer'> <img src={currentFlag.flagURL} alt="" /></div>
       }
+      {fourChoicesShuffled.map ((oneOption=> {
+        return <div key={oneOption._id}> {oneOption.name}</div>
+      })) }
     </div>
   )
 }
