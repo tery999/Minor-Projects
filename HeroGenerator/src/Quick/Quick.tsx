ae059e4059f18ec8adcb9bf9cadf1f09b9d41a03
tery999 <51335465+tery999@users.noreply.github.com>
2024-05-27 12:38:29 +0300

    Futark, Ogam, Sirilsko males done
    Skipping Latinea, as family name combines father ( male ) and mother (female) names, and I dont have the female names done yet.

--- HeroGenerator/src/Quick/Quick.tsx
+++ HeroGenerator/src/Quick/Quick.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { QuickHero } from "../Interfaces/QuickHero"
+import { Link } from "react-router-dom"
 import { genderGenerator, nameGenerator, originGenerator, societyGenerator } from "../functions/GeneratorFunc"
 
 export default function Quick () {
@@ -11,8 +12,10 @@ export default function Quick () {
     })
 
     useEffect( ()=> {
-        const genderGen = genderGenerator();
-        const origin = originGenerator();
+        // const genderGen = genderGenerator();
+        // const origin = originGenerator("quick");
+        const genderGen = "мъж";
+        const origin = "Сирильско краище";
         const society = societyGenerator();
         const name = nameGenerator(genderGen, origin)
         setHeroStats( (prev)=> ({...prev, gender: genderGen , origin: origin, society:society, name:name}))
@@ -20,6 +23,7 @@ export default function Quick () {
 
     return (
         <div className="container">
+            <Link to="/">Назад</Link>
             <p> Пол: {heroStats.gender}</p>
             <p> Произход: {heroStats.origin}</p>
             <p> Общество: {heroStats.society}</p>
--- HeroGenerator/src/functions/GeneratorFunc.ts
+++ HeroGenerator/src/functions/GeneratorFunc.ts
@@ -3,7 +3,7 @@ import { FutarkMale, HabileaMale, LatinaeMale, OgamMale, OrkhongMale, SirilskoMa
 import { origin, society } from "../Interfaces/QuickHero";
 
 export function genderGenerator(): "мъж" | "жена" {
-    const diceRoll = Math.floor(Math.random() * 101)+1;
+    const diceRoll = Math.floor(Math.random() * 101) + 1;
     if (diceRoll <= 50) {
         const gender = "мъж"
         return gender;
@@ -12,9 +12,16 @@ export function genderGenerator(): "мъж" | "жена" {
         return gender;
     }
 }
-
-export function originGenerator(): origin {
-    const diceRoll = Math.floor(Math.random() * 101)+1;
+// options act as pointers, whether or not there is option for the person to 
+//pick their own stat , if they are lucky and roll higher than 90
+export function originGenerator(option:"quick"|"complex"): origin {
+    let diceRoll:number = 0;
+    if (option === "quick") {
+        diceRoll = Math.floor(Math.random() * 100) + 1;
+    } else if (option ==="complex") {
+        diceRoll = Math.floor(Math.random() * 90) + 1;
+    }
+   
     let randomChoice: origin = ""
     switch (true) {
         case (diceRoll <= 15):
@@ -32,10 +39,10 @@ export function originGenerator(): origin {
         case (diceRoll >= 61 && diceRoll <= 75):
             randomChoice = "Орхонг";
             break;
-            case (diceRoll >= 76 && diceRoll <= 90):
+        case (diceRoll >= 76 && diceRoll <= 90):
             randomChoice = "Хабилеа";
             break;
-            case (diceRoll >= 91):
+        case (diceRoll >= 91):
             randomChoice = "";
             break;
 
@@ -43,12 +50,12 @@ export function originGenerator(): origin {
     return randomChoice;
 }
 
-export function societyGenerator():society {
-    const diceRoll = Math.floor(Math.random() * 101)+1;
-    let society:society = "";
+export function societyGenerator(): society {
+    const diceRoll = Math.floor(Math.random() * 100) + 1;
+    let society: society = "";
     if (diceRoll <= 25) {
         society = "племенно"
-    } else if (diceRoll >= 26 && diceRoll <=75 ){
+    } else if (diceRoll >= 26 && diceRoll <= 75) {
         society = "феодално"
     } else {
         society = "индустриално"
@@ -56,40 +63,79 @@ export function societyGenerator():society {
     return society;
 }
 
-export function nameGenerator(gender:"мъж"|"жена" , origin:origin) {
-    const diceRoll = Math.floor(Math.random() * 51)+1;
-    let name:string = "";
-    if ( gender === "мъж") {
+export function nameGenerator(gender: "мъж" | "жена", origin: origin) {
+    //given name
+    const diceRoll = Math.floor(Math.random() * 50) + 1;
+    //second name
+    const diceRollSecond = Math.floor(Math.random() * 50) + 1;
+    //third name or clan
+    const diceRollThird = Math.floor(Math.random() * 50) + 1;
+    let name: string = "";
+    let secName: string = "";
+    let trdName:string = "";
+    let fullName:string = "";
+    if (gender === "мъж") {
         if (origin === "Футарк") {
             name = FutarkMale[diceRoll];
-        } else  if (origin==="Огам") {
+            secName = FutarkMale[diceRollSecond] + "сон";
+            trdName = FutarkMale[diceRollThird];
+            const VonChance = Math.floor(Math.random() * 100) + 1;
+            if (VonChance >= 51) {
+                fullName = `${name} ${secName} Вон ${trdName}`
+                return fullName;
+            } else {
+                fullName = `${name} ${secName}`;
+            }
+
+        } else if (origin === "Огам") {
             name = OgamMale[diceRoll];
-        } else if ( origin==="Сирильско краище") {
+            secName = OgamMale[diceRollSecond]
+            // Mac, Of, O
+            const ogamNameChance = Math.floor(Math.random() * 99) + 1;
+            if (ogamNameChance <= 33) {
+                fullName = `${name} Мак${secName.toLowerCase()}`;
+            } else if ( ogamNameChance >= 34 && ogamNameChance <= 66) {
+                fullName = `${name} О${secName.toLowerCase()}`;
+            } else {
+                fullName = `${name} ъв ${secName}`;
+            }
+        } else if (origin === "Сирильско краище") {
             name = SirilskoMale[diceRoll];
-        } else if ( origin==="Латинае") {
+            secName = SirilskoMale[diceRollSecond]
+            // -ов, -овски -ович
+            const sirilNameChance = Math.floor(Math.random() * 99) + 1;
+            if (sirilNameChance <= 33) {
+                fullName = `${name} ${secName}ов`;
+            } else if ( sirilNameChance >= 34 && sirilNameChance <= 66) {
+                fullName = `${name} ${secName}овски`;
+            } else {
+                fullName = `${name} ${secName}ович`;
+            }
+        } else if (origin === "Латинае") {
+            //LATER NEED FEMALE NAMES TO CREATE FULL NAME
             name = LatinaeMale[diceRoll];
-        } else if (origin==="Орхонг") {
+        } else if (origin === "Орхонг") {
             name = OrkhongMale[diceRoll];
         } else {
             name = HabileaMale[diceRoll];
         }
     }
-    else if (gender ==="жена") {
+    else if (gender === "жена") {
         if (origin === "Футарк") {
             name = FutarkFemale[diceRoll];
-        } else  if (origin==="Огам") {
+        } else if (origin === "Огам") {
             name = OgamFemale[diceRoll];
-        } else if ( origin==="Сирильско краище") {
+        } else if (origin === "Сирильско краище") {
             name = SirilskoFemale[diceRoll];
-        } else if ( origin==="Латинае") {
+        } else if (origin === "Латинае") {
             name = LatinaeFemale[diceRoll];
-        } else if (origin==="Орхонг") {
+        } else if (origin === "Орхонг") {
             name = OrkhongFemale[diceRoll];
         } else {
             name = HabileaFemale[diceRoll];
         }
     }
-    return name;
+    return fullName;
 }
 
 
