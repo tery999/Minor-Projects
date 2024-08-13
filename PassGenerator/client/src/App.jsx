a109523360a5d2b2ee2d09e331a0254c25c1253f
tery999 <51335465+tery999@users.noreply.github.com>
2024-08-13 22:09:36 +0300

    more logic
    mainly focused on reading about modular css. Learned some things which were troublesome in the past

--- PassGenerator/client/src/App.jsx
+++ PassGenerator/client/src/App.jsx
@@ -14,20 +14,28 @@ function App() {
       for ( let i = 0 ; i < 6 ; i++) {
         generatedPassword+= letterGenerator();
       }
-    }
+    } else if ( passLength==="Medium") {
+        for ( let i = 0 ; i < 12 ; i++) {
+          generatedPassword+= letterGenerator();
+        }
+    } else if ( passLength==="Big") {
+      for ( let i = 0 ; i < 20 ; i++) {
+        generatedPassword+= letterGenerator();
+      }
+  }
 
     setPassword(generatedPassword);
   }
   return (
     <div className={styles.holder}>
       <div className={styles.options}>
-        <button className={styles.button}> UpperCase</button>
-        <button className={styles.button}> Numbers</button>
+        <button className={`${styles.button} ${upperCase ? styles.clicked : ""}`} onClick={()=>setUpperCase(prev => !prev)}> UpperCase</button>
+        <button className={`${styles.button} ${numbers ? styles.clicked : ""}`} onClick={()=>setNumbers(prev => !prev)}> Numbers</button>
       </div>
       <div className={styles.lengthOption}>
-        <button className={styles.button}> Small</button>
-        <button className={styles.button}> Medium</button>
-        <button className={styles.button}> Big</button>
+        <button className={`${styles.button} ${passLength==="Small" ? styles.clicked : ""}`} onClick={()=>setPassLength("Small")}> Small</button>
+        <button className={`${styles.button} ${passLength==="Medium" ? styles.clicked : ""}`} onClick={()=>setPassLength("Medium")}> Medium</button>
+        <button className={`${styles.button} ${passLength==="Big" ? styles.clicked : ""}`} onClick={()=>setPassLength("Big")}> Big</button>
       </div>
       <div>
         <button onClick={generatePasswordClickFunc} className={`${styles.button} ${styles.generate}`}>GENERATE</button>
